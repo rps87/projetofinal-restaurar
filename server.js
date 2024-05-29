5d71a4a7c7fd1a9e343a4ab5ea99ef6dac06b31d
Raphael Santos <rps.indio@gmail.com>
2024-05-29 20:01:03 -0300

    quarto commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {  
-    res.send("PROJETO FINAL");
+    res.send("PROJETO FINAL - v1");
 });
 app.listen(3000, () => {  
     console.log("Server running on 3000");
